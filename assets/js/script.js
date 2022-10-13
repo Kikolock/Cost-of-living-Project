09d75d968d344f361036be1a70552783682ffd8b
Kikolock <icaro12@hotmail.com>
2022-10-13 19:40:15 -0400

    Adding search bar working on consoleLog

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,27 +15,37 @@ var formSubmitHandler = function (event) {
 
 	console.log({ cityName, countryName })
 
-	if (cityName.value && countryName.value) {
-		getCityName(cityName.value);
+	if (cityName && countryName) {
+		getCityName(cityName, countryName);
 		cityName.innerText = '';
 		countryName.innerText = '';
-	};
+	}
+	//  else {
+	// 	alert("Please enter a City and Country name");
+	// }
+	
 
-	// 	if(countryName.value) {
-	// 		getCityName(cityName.value.trim());
-	// 		countryName.innerText = ''; 
-	// 	} else {
-	// 		alert("Please enter a City and Country name");
-	// 	}
-	// };
 }
 
 
+// function renderResults(results) {
+//     const list = document.getElementById('resultsList');
+//     results.forEach(result => {
+//         const element = document.createElement("li");
+//         element.innerText = result;
+//         list.appendChild(element);
+//     });
+// }
 
+
+
+// // displayPrices(response.prices)
+// const results = jsonData.map(element => element.prices)
+// 	    renderResults(results);
 
 function getCityName(cityName, countryName) {
 
-	console.log(city, country)
+	console.log(cityName, countryName)
 	
 	const optionsForPrices = {
 		method: 'GET',
@@ -51,5 +61,19 @@ function getCityName(cityName, countryName) {
 	.catch(err => console.error(err));
 }
 
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '9e4ee344femsh6a6226c9bee49aap12340ejsnef8844927025',
+// 		'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=Toronto&country_name=Canada', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
 cityFormEl.addEventListener("click", formSubmitHandler);
