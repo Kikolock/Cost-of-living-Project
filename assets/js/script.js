7624f29362f1e04aca2d5ade50686c526780714b
andres192x <andres192x@hotmail.com>
2022-10-14 15:10:39 -0500

    added table of contents, categories, innerhtml under categories with names and prices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,24 @@
+
+
 var cityFormEl = document.querySelector("#city-form");
-// // var headerWrapperEl = document.querySelector("#header-wrapper");
-// // var searchFormEl = document.querySelector("#search-form")
-// // var displayCity = document.querySelector("#display-city")
-// // var cityContainerEl = document.querySelector("#city-container") 
-// // var comparisonButtonEl = document.querySelector("#comparison-buttons")
+
 
 var formSubmitHandler = function (event) {
 	event.preventDefault();
-	//get value from the input element
-	var cityName = document.getElementById('cityName').value; 
-	//console.log(cityName.value.trim())
-
-	var countryName = document.getElementById('countryName').value;
-
-	console.log({ cityName, countryName })
-
-	if (cityName && countryName) {
-		getCityName(cityName, countryName);
-		cityName.innerText = '';
-		countryName.innerText = '';
-	}
-	//  else {
-	// 	alert("Please enter a City and Country name");
-	// }
 	
+	var cityName = document.getElementById('cityName'); 
+	var countryName = document.getElementById('countryName');
 
+		if (cityName && countryName) {
+		getCityName(cityName.value, countryName.value);
+		cityName.value = '';
+		countryName.value = '';
+	}
+	
 }
-
-
-// function renderResults(results) {
-//     const list = document.getElementById('resultsList');
-//     results.forEach(result => {
-//         const element = document.createElement("li");
-//         element.innerText = result;
-//         list.appendChild(element);
-//     });
-// }
-
-
-
-// // displayPrices(response.prices)
-// const results = jsonData.map(element => element.prices)
-// 	    renderResults(results);
 
 function getCityName(cityName, countryName) {
 
-	console.log(cityName, countryName)
 	
 	const optionsForPrices = {
 		method: 'GET',
@@ -55,25 +28,131 @@ function getCityName(cityName, countryName) {
 		}
 	};
 	
+	
 	fetch('https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=' + cityName + '&country_name=' + countryName, optionsForPrices)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(json => { //obtain data
+	let data = {};
+	for (const item of json.prices) {
+	
+		if (!data[item.category_name]){
+			data[item.category_name]= [];
+		}
+		data[item.category_name].push(item);
+	}
+	
+	const ElementUl = document.getElementById('table-contents');
+	ElementUl.innerHTML= ""
+	for (const category in data) {
+		ElementUl.insertAdjacentHTML("beforeend",`<li> ${category} </li>`)
+	}
+
+	const ElementCat = document.getElementById('category-contents');
+	ElementCat.innerHTML= ""
+	
+	for (const category in data) {
+		let listItem= ""
+		for (const item of data[category]) {
+		
+			listItem= listItem+`<li>${item.item_name}-<b>$ ${item.avg}</b></li>`
+		}
+
+		ElementCat.insertAdjacentHTML("beforeend",`<div>
+		<h3>${category}</h3>
+		<ul>${listItem}		  
+		</ul>
+	  </div>`)
+
+		
+	  }
+
+
+	})
 	.catch(err => console.error(err));
 }
 
 
+cityFormEl.addEventListener("submit", formSubmitHandler);
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '9e4ee344femsh6a6226c9bee49aap12340ejsnef8844927025',
-// 		'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+
+
+
+// var cityFormEl = document.querySelector("#city-form");
+// // // var headerWrapperEl = document.querySelector("#header-wrapper");
+// // // var searchFormEl = document.querySelector("#search-form")
+// // // var displayCity = document.querySelector("#display-city")
+// // // var cityContainerEl = document.querySelector("#city-container") 
+// // // var comparisonButtonEl = document.querySelector("#comparison-buttons")
+
+// var formSubmitHandler = function (event) {
+// 	event.preventDefault();
+// 	//get value from the input element
+// 	var cityName = document.getElementById('cityName').value; 
+// 	//console.log(cityName.value.trim())
+
+// 	var countryName = document.getElementById('countryName').value;
+
+// 	console.log({ cityName, countryName })
+
+// 	if (cityName && countryName) {
+// 		getCityName(cityName, countryName);
+// 		cityName.innerText = '';
+// 		countryName.innerText = '';
 // 	}
-// };
+// 	//  else {
+// 	// 	alert("Please enter a City and Country name");
+// 	// }
+	
 
-// fetch('https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=Toronto&country_name=Canada', options)
+// }
+
+
+// // function renderResults(results) {
+// //     const list = document.getElementById('resultsList');
+// //     results.forEach(result => {
+// //         const element = document.createElement("li");
+// //         element.innerText = result;
+// //         list.appendChild(element);
+// //     });
+// // }
+
+
+
+// // // displayPrices(response.prices)
+// // const results = jsonData.map(element => element.prices)
+// // 	    renderResults(results);
+
+// function getCityName(cityName, countryName) {
+
+// 	console.log(cityName, countryName)
+	
+// 	const optionsForPrices = {
+// 		method: 'GET',
+// 		headers: {
+// 			'X-RapidAPI-Key': '9e4ee344femsh6a6226c9bee49aap12340ejsnef8844927025',
+// 			'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+// 		}
+// 	};
+	
+// 	fetch('https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=' + cityName + '&country_name=' + countryName, optionsForPrices)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
+// }
 
-cityFormEl.addEventListener("click", formSubmitHandler);
+
+
+// // const options = {
+// // 	method: 'GET',
+// // 	headers: {
+// // 		'X-RapidAPI-Key': '9e4ee344femsh6a6226c9bee49aap12340ejsnef8844927025',
+// // 		'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+// // 	}
+// // };
+
+// // fetch('https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=Toronto&country_name=Canada', options)
+// // 	.then(response => response.json())
+// // 	.then(response => console.log(response))
+// // 	.catch(err => console.error(err));
+
+// cityFormEl.addEventListener("click", formSubmitHandler);
